92a3b47051f4f6710f02ff9d6d8d22ca520b41ba
Xhino Mullaymeri <s223481@dtu.dk>
2025-01-13 02:37:40 +0100

    Adds download_dataset function and integrates it in preprocess.

--- src/plant_leaves/data.py
+++ src/plant_leaves/data.py
@@ -8,6 +8,113 @@ from PIL import Image
 from typing import List, Tuple
 import torch
 from torchvision import transforms
+import kagglehub
+
+
+data_typer = typer.Typer()
+
+@data_typer.command()
+def download_dataset(
+    dataset: str = typer.Argument("csafrit2/plant-leaves-for-image-classification", help="Kaggle dataset identifier"),
+    destination: str = typer.Argument("../../data/raw", help="Destination folder for the dataset")
+) -> None:
+    """
+    Download the dataset from Kaggle. Kaggle API must be installed and configured (https://www.kaggle.com/docs/api#authentication).
+    Note: The dataset is downloaded to the kagglehub cache folder and then moved to the destination folder.
+
+        Parameters:
+            dataset (str, optional): Default value is "csafrit2/plant-leaves-for-image-classification".
+            destination (str, optional): Default value is "../../data/raw".
+            
+        Returns:
+            None
+    """
+    try:
+        kagglehub.whoami()
+    except Exception as e:
+        print("Please setup Kaggle API credentials first (using kaggle.json). Check https://www.kaggle.com/docs/api#authentication")
+        return
+    # When download_dataset is called from another function with defaults, the arguments are ArgumentInfo objects
+    if isinstance(dataset, typer.models.ArgumentInfo):
+        dataset = dataset.default
+    if isinstance(destination, typer.models.ArgumentInfo):
+        destination = destination.default
+    dataset_folder_name = dataset.split("/")[-1]
+    destination = os.path.join(destination, dataset_folder_name)
+    destination = os.path.normpath(destination)
+
+    if os.path.exists(destination):
+        print(f"Dataset {dataset} already downloaded in {destination}")
+        return
+    
+    try:
+        path = kagglehub.dataset_download(dataset)
+        path = os.path.normpath(path)
+        print("Path (kagglehub cache) to dataset files:", path)
+        os.rename(path, destination)
+        print("Files moved to:", destination)
+    except Exception as e:
+        print(f"Error downloading or moving dataset: {e}")
+
+
+@data_typer.command()
+def preprocess(
+    raw_data_path: Path = typer.Argument(
+        default=Path("../../data/raw/plant-leaves-for-image-classification\Plants_2"), help="Path to the folder containing raw data."
+    ),
+    output_folder: Path = typer.Argument(
+        default=Path("../../data/processed"), help="Path to the folder where processed data will be stored."
+    ),
+    dimensions: tuple[int, int] = typer.Option(
+        (240, 240), help="Target dimensions for image resizing (width, height)."
+    )
+) -> None:
+    """
+    Preprocess the raw data and save the processed data in the output folder.
+
+        Parameters:
+            raw_data_path: Path to the folder containing raw data.
+            output_folder: Path to the folder where processed data will be stored.
+            dimensions: Target dimensions for image resizing (width, height).
+
+        Returns:
+            None
+    """
+    print("Checking if raw data folder exists...")
+    if not raw_data_path.exists():
+        print("The raw data folder does not exist. Downloading the dataset...")
+        download_dataset() # TODO: Add arguments properly. Issue: raw_data_path links to data folder and not to coockie cutter raw data folder.
+    if not raw_data_path.exists(): # If the download failed, exit.
+        print("Download failed. Exiting...")
+        exit()
+
+    print("Preprocessing data...")
+    for dataset_path in raw_data_path.iterdir():
+        if dataset_path.name == "images to predict":
+            continue
+        # Check if the folder exists and if not create it
+        if not os.path.exists(os.path.join(output_folder, dataset_path.name)):
+            os.makedirs(os.path.join(output_folder, dataset_path.name))
+    
+        dataset_path = Path(dataset_path)
+        output_path = Path(os.path.join(output_folder, dataset_path.name))
+        print(dataset_path, output_path)
+        if dataset_path.name == "train":
+            main_preprocessing(dataset_path, output_path, dimensions=dimensions)
+        else:
+            main_preprocessing(dataset_path, output_path)
+
+
+def normalize(images: torch.Tensor) -> torch.Tensor:
+    """Normalize images as (X - mean(X)) / std(X).
+
+    Parameters:
+    - images: Tensor of shape (N, 3, 244 or 288, 244 or 288)
+
+    Returns:
+    - Normalized images
+    """
+    return (images - images.mean()) / images.std()
 
 
 def main_preprocessing(data_path: Path, output_path: Path, dimensions: Tuple[int,int] = [288, 288]) -> None:
@@ -23,7 +130,6 @@ def main_preprocessing(data_path: Path, output_path: Path, dimensions: Tuple[int
     # Extract images to output folders
     for folder_path in data_path.iterdir():
         for img_path in folder_path.iterdir():
-            print(img_path)
             if img_path.suffix.lower() in [".jpg", ".jpeg", ".png"]:
                 category = "healthy" if "healthy" in img_path.name else "diseased"
                 
@@ -39,7 +145,6 @@ def main_preprocessing(data_path: Path, output_path: Path, dimensions: Tuple[int
                     datasets_pt.append(tensor)
                     targets_pt.append(1)
 
-    
     # Transform lists to tensors
     datasets_pt = torch.stack(datasets_pt)
     targets_pt = torch.tensor(targets_pt)
@@ -50,36 +155,7 @@ def main_preprocessing(data_path: Path, output_path: Path, dimensions: Tuple[int
     # Save the datasets and targets as .pt files
     torch.save(datasets_pt, output_path / "datasets.pt")
     torch.save(targets_pt, output_path / "targets.pt")
-                
 
-def normalize(images: torch.Tensor) -> torch.Tensor:
-    """Normalize images as (X - mean(X)) / std(X).
-
-    Parameters:
-    - images: Tensor of shape (N, 3, 244 or 288, 244 or 288)
-
-    Returns:
-    - Normalized images
-    """
-    return (images - images.mean()) / images.std()
-
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    for dataset_path in raw_data_path.iterdir():
-        if dataset_path.name == "images to predict":
-            continue
-        # Check if the folder exists and if not create it
-        if not os.path.exists(os.path.join(output_folder, dataset_path.name)):
-            os.makedirs(os.path.join(output_folder, dataset_path.name))
-    
-        dataset_path = Path(dataset_path)
-        output_path = Path(os.path.join(output_folder, dataset_path.name))
-        print(dataset_path, output_path)
-        if dataset_path.name == "train":
-            main_preprocessing(dataset_path, output_path, dimensions=[240, 240])
-        else:
-            main_preprocessing(dataset_path, output_path)
-    
 
 def load_processed_data(processed_data_path: Path) -> Tuple[torch.utils.data.TensorDataset, torch.utils.data.TensorDataset, torch.utils.data.TensorDataset]:
 
@@ -100,5 +176,4 @@ def load_processed_data(processed_data_path: Path) -> Tuple[torch.utils.data.Ten
 
 
 if __name__ == "__main__":
-    # typer.run(preprocess)
-    typer.run(preprocess)
+    data_typer()
