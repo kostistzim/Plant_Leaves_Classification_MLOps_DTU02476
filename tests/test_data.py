19c51df26bdfffc4afc2cfd204eaa8f9c4470f12
Jan Sulicek <sulicek.jan@seznam.cz>
2025-01-20 16:43:43 +0100

    add process_dataset tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,14 +1,46 @@
+import shutil
+from pathlib import Path
+
 import torch
 
-from plant_leaves.data import MyDataset, load_processed_data
-from tests import _PATH_DATA
+from plant_leaves.data import load_processed_data, preprocess
+from tests import _PATH_DATA, _PATH_TEST_DATA
+
+# def test_data():
+#     # load datset
+#     print(_PATH_DATA)
+#     train_set, test_set, validation_set = load_processed_data(_PATH_DATA)
+#     for dataset, target in [(train_set, 4274), (test_set, 110), (validation_set, 110)]:
+#         assert len(dataset) == target, "Wrong number of samples in dataset"
+#         x, _ = dataset[0]
+#         assert x.shape == (3, 240, 240) or x.shape == (3, 288, 288)
 
 
-def test_data():
-    # load datset
-    print(_PATH_DATA)
-    train_set, test_set, validation_set = load_processed_data(_PATH_DATA)
-    for dataset, target in [(train_set, 4274), (test_set, 110), (validation_set, 110)]:
-        assert len(dataset) == target, "Wrong number of samples in dataset"
-        x, _ = dataset[0]
-        assert x.shape == (3, 240, 240) or x.shape == (3, 288, 288)
+def test_preprocess_and_load_data():
+    processed_data_path = _PATH_TEST_DATA.joinpath("processed")
+    # Delete processed data if exists
+    if processed_data_path.exists():
+        shutil.rmtree(processed_data_path)
+
+    # Call preprocess
+    preprocess(
+        _PATH_TEST_DATA.joinpath("raw/plant-leaves-for-image-classification/Plants_2"), processed_data_path, (240, 240)
+    )
+
+    # Load preprocessed images
+    loaded_images = []
+    loaded_targets = []
+
+    loaded_images.append(torch.load(processed_data_path / "train" / "datasets.pt", weights_only=True))
+    loaded_images.append(torch.load(processed_data_path / "test" / "datasets.pt", weights_only=True))
+    loaded_images.append(torch.load(processed_data_path / "valid" / "datasets.pt", weights_only=True))
+
+    loaded_targets.append(torch.load(processed_data_path / "train" / "targets.pt", weights_only=True))
+    loaded_targets.append(torch.load(processed_data_path / "valid" / "targets.pt", weights_only=True))
+    loaded_targets.append(torch.load(processed_data_path / "test" / "targets.pt", weights_only=True))
+
+    # Assert count and shape
+    for image in loaded_images:
+        assert image.shape == torch.Size([8, 3, 240, 240]) or image.shape == torch.Size([8, 3, 288, 288])
+    for target in loaded_targets:
+        assert target.shape == torch.Size([8])
