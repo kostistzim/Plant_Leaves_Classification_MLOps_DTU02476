1bfd20d17d3e03052dd79c416619f1ac2e7f1613
michalisdikaiopoulos <michalisdikaiopoulos@gmail.com>
2025-01-22 18:16:44 +0100

    Configure model to onnx graph

--- src/plant_leaves/__init__.py
+++ src/plant_leaves/__init__.py
@@ -1,4 +1,6 @@
 import os
 import sys
+from pathlib import Path
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
--- src/plant_leaves/config/logging_config.py
+++ src/plant_leaves/config/logging_config.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+
 from loguru import logger
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 
 def configure_logger():
     logger.configure(extra={"prefix": "DEFAULT"})
     logger.remove(0)
-    logger.add("logs/logging.log", format="[{extra[prefix]}] | {time:MMMM D, YYYY > HH:mm:ss} | {level} | {message}")
+    logger.add(
+        f"{PROJECT_ROOT}logs/logging.log",
+        format="[{extra[prefix]}] | {time:MMMM D, YYYY > HH:mm:ss} | {level} | {message}",
+    )
 
 
 configure_logger()
