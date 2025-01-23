6cb77c80bbf9901371d88e46196fe3e33c287847
Jan Sulicek <sulicek.jan@seznam.cz>
2025-01-23 16:49:31 +0100

    update test_data

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,16 +7,6 @@ from plant_leaves.data import load_processed_data, preprocess
 from tests import _PATH_DATA, _PATH_TEST_DATA
 
 
-def test_load_data():
-    # load datset
-    print(_PATH_DATA)
-    train_set, test_set, validation_set = load_processed_data(_PATH_DATA)
-    for dataset, target in [(train_set, 4274), (test_set, 110), (validation_set, 110)]:
-        assert len(dataset) == target, "Wrong number of samples in dataset"
-        x, _ = dataset[0]
-        assert x.shape == (3, 240, 240) or x.shape == (3, 288, 288)
-
-
 def test_preprocess_data():
     processed_data_path = _PATH_TEST_DATA.joinpath("processed")
     # Delete processed data if exists
@@ -42,6 +32,15 @@ def test_preprocess_data():
 
     # Assert count and shape
     for image in loaded_images:
-        assert image.shape == torch.Size([8, 3, 240, 240]) or image.shape == torch.Size([8, 3, 288, 288])
+        assert image.shape == torch.Size([2, 3, 240, 240]) or image.shape == torch.Size([2, 3, 288, 288])
     for target in loaded_targets:
-        assert target.shape == torch.Size([8])
+        assert target.shape == torch.Size([2])
+
+
+def test_load_data():
+    # load datset
+    train_set, test_set, validation_set = load_processed_data(_PATH_TEST_DATA.joinpath("processed"))
+    for dataset, target in [(train_set, 2), (test_set, 2), (validation_set, 2)]:
+        assert len(dataset) == target, "Wrong number of samples in dataset"
+        x, _ = dataset[0]
+        assert x.shape == (3, 240, 240) or x.shape == (3, 288, 288)
